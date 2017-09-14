1dd7bfc9c9f7ce511e6a7696ac33fb87dc595a1c
razborges <razborges@me.com>
2017-09-14 13:57:26 -0300

    aula componentes

---
+++ src/app/app.component.js
@@ -0,0 +1,15 @@
+// @ string
+// = two way databinding
+// < one way databinding
+// & function
+angular
+  .module('app')
+  .component('hero', {
+    templateUrl: 'app/hero.html',
+    bindings: {
+      hero: '<'
+    },
+    controller: function() {
+      this.hero = 'This is a new component from Angular';
+    }
+  });
