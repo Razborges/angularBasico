387254a16cdaff37a6b6c917655b82a768a5ab85
razborges <razborges@me.com>
2017-09-14 11:46:08 -0300

    aula controller

--- src/app/hello.js
+++ src/app/hello.js
@@ -4,5 +4,13 @@ angular
     templateUrl: 'app/hello.html',
     controller: function () {
       this.hello = 'Hello World!';
+      this.person = {
+        name: 'TesteName',
+        lastname: 'TesteSobre'
+      };
+
+      this.alertMe = function() {
+        alert(this.person.name + ' ' + this.person.lastname);
+      }
     }
   });
